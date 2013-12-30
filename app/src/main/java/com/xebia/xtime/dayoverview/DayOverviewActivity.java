a8db47c5ba75d851607683ec1e010b346ea19c96
Steven Mulder <steven.mulder@gmail.com>
2013-12-30 22:08:10 +0100

    Added date in title of DayOverviewActivity

--- app/src/main/java/com/xebia/xtime/dayoverview/DayOverviewActivity.java
+++ app/src/main/java/com/xebia/xtime/dayoverview/DayOverviewActivity.java
@@ -2,6 +2,7 @@ package com.xebia.xtime.dayoverview;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 
 import com.xebia.xtime.R;
@@ -9,11 +10,14 @@ import com.xebia.xtime.editor.EditTimeCellActivity;
 import com.xebia.xtime.shared.model.DayOverview;
 import com.xebia.xtime.shared.model.TimeSheetEntry;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 public class DayOverviewActivity extends ActionBarActivity implements DailyTimeSheetFragment
         .Listener {
 
-    public static final String EXTRA_DAY_OVERVIEW = "com.xebia.xtime.dayoverview.DAY_OVERVIEW";
-    public static final String EXTRA_WEEK_OVERVIEW = "com.xebia.xtime.dayoverview.WEEK_OVERVIEW";
+    public static final String EXTRA_DAY_OVERVIEW = "day_overview";
+    public static final String EXTRA_WEEK_OVERVIEW = "week_overview";
     private static final int REQ_CODE_EDIT = 1;
 
     @Override
@@ -21,11 +25,21 @@ public class DayOverviewActivity extends ActionBarActivity implements DailyTimeS
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_overview);
 
-        if (savedInstanceState == null) {
-            DayOverview dayOverview = getIntent().getParcelableExtra(EXTRA_DAY_OVERVIEW);
-            getSupportFragmentManager().beginTransaction().add(R.id.container,
-                    DailyTimeSheetFragment.getInstance(dayOverview)).commit();
+        // get the day overview
+        DayOverview dayOverview = getIntent().getParcelableExtra(EXTRA_DAY_OVERVIEW);
+        if (null == dayOverview) {
+            throw new NullPointerException("Missing EXTRA_DAY_OVERVIEW");
         }
+
+        // set up the UI
+        if (savedInstanceState == null) {
+            Fragment fragment = DailyTimeSheetFragment.getInstance(dayOverview);
+            getSupportFragmentManager().beginTransaction().add(R.id.container, fragment).commit();
+        }
+
+        // set up the title
+        DateFormat dateFormat = new SimpleDateFormat("EEE d MMMM");
+        setTitle(dateFormat.format(dayOverview.getDate()));
     }
 
     @Override
