cb6f2269b82b5fbf81bfac134b25b969c56ce2ae
Steven Mulder <steven.mulder@gmail.com>
2013-12-31 17:36:37 +0100

    Updated styling for week overview list.
    Highlight today's entry using Xebia purple background, and use gray text color for future days

--- app/src/main/java/com/xebia/xtime/weekoverview/DailyHoursListAdapter.java
+++ app/src/main/java/com/xebia/xtime/weekoverview/DailyHoursListAdapter.java
@@ -2,6 +2,9 @@ package com.xebia.xtime.weekoverview;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +14,12 @@ import android.widget.TextView;
 import com.xebia.xtime.R;
 import com.xebia.xtime.shared.model.DayOverview;
 
+import java.text.DateFormat;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 /**
  * Adapter that displays DayOverview objects in a list. Each list row contains a date indicator
@@ -21,8 +27,17 @@ import java.util.List;
  */
 public class DailyHoursListAdapter extends ArrayAdapter<DayOverview> {
 
+    private final DateFormat mDateFormat;
+    private final int mXebiaPurple;
+
     public DailyHoursListAdapter(Context context, List<DayOverview> data) {
         super(context, R.layout.row_daily_hours, R.id.hours, data);
+
+        mDateFormat = new SimpleDateFormat("EEE d MMM");
+        mDateFormat.setTimeZone(TimeZone.getTimeZone("CET"));
+
+        Resources res = context.getResources();
+        mXebiaPurple = res.getColor(R.color.xebia_purple);
     }
 
     @Override
@@ -38,14 +53,24 @@ public class DailyHoursListAdapter extends ArrayAdapter<DayOverview> {
         // find the view
         if (row != null) {
             TextView dateView = (TextView) row.findViewById(R.id.date);
-            TextView monthView = (TextView) row.findViewById(R.id.month);
             TextView hoursView = (TextView) row.findViewById(R.id.hours);
+            TextView hoursLabelView = (TextView) row.findViewById(R.id.hours_label);
 
             // update the view content
             DayOverview item = getItem(position);
-            dateView.setText(new SimpleDateFormat("dd").format(item.getDate()));
-            monthView.setText(new SimpleDateFormat("MMM").format(item.getDate()));
+            Date date = item.getDate();
+            dateView.setText(mDateFormat.format(date));
             hoursView.setText(NumberFormat.getNumberInstance().format(item.getTotalHours()));
+            if (DateUtils.isToday(date.getTime())) {
+                row.setBackgroundColor(mXebiaPurple);
+                dateView.setTextColor(Color.WHITE);
+                hoursView.setTextColor(Color.WHITE);
+                hoursLabelView.setTextColor(Color.WHITE);
+            } else if (date.getTime() > new Date().getTime()) {
+                dateView.setTextColor(Color.GRAY);
+                hoursView.setTextColor(Color.GRAY);
+                hoursLabelView.setTextColor(Color.GRAY);
+            }
         }
 
         return row;
