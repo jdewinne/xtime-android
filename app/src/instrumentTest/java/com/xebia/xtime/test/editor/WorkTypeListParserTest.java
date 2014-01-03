7664c88e18b440297e2ac81ae26957ac13ee58ad
Steven Mulder <steven.mulder@gmail.com>
2014-01-03 08:29:02 +0100

    Expanded test cases for parsers to check long list handling.

--- app/src/instrumentTest/java/com/xebia/xtime/test/editor/WorkTypeListParserTest.java
+++ app/src/instrumentTest/java/com/xebia/xtime/test/editor/WorkTypeListParserTest.java
@@ -13,22 +13,26 @@ public class WorkTypeListParserTest extends TestCase {
     private static final String INPUT_REGULAR = "throw 'allowScriptTagRemoting is false.';\n" +
             "//#DWR-INSERT\n" +
             "//#DWR-REPLY\n" +
-            "var s0=[];var s1=[];var s2={};var s3={};var s4={};var s5={};var s6={};s0[0]=false;" +
-            "s0[1]=false;s0[2]=false;s0[3]=false;s0[4]=false;s0[5]=false;s0[6]=false;\n" +
-            "s1[0]=s2;s1[1]=s3;s1[2]=s4;s1[3]=s5;s1[4]=s6;\n" +
+            "var s0=[];var s1=[];var s2={};var s3={};var s4={};var s5={};var s6={};s0[0]=false;s0[1]=false;s0[2]=false;s0[3]=false;s0[4]=false;s0[5]=false;s0[6]=false;\n" +
+            "s1[0]=s2;s1[1]=s3;s1[2]=s4;s1[3]=s5;s1[4]=s6;s1[5]=s7;s1[6]=s8;s1[7]=s9;s1[8]=s10;s1[9]=s11;s1[10]=s12;\n" +
             "s2.workType=\"400\";s2.workTypeDescription=\"Fixed Price Consultancy\";\n" +
             "s3.workType=\"420\";s3.workTypeDescription=\"Consultancy on Maintenance\";\n" +
             "s4.workType=\"430\";s4.workTypeDescription=\"Free work/ cost for  Xebia\";\n" +
             "s5.workType=\"450\";s5.workTypeDescription=\"Trainings (given)\";\n" +
             "s6.workType=\"455\";s6.workTypeDescription=\"Trainings (preparation)\";\n" +
-            "dwr.engine._remoteHandleCallback('4','0',{dayEnabledStatus:s0," +
-            "workTypeWorkGroupList:s1});";
+            "s7.workType=\"500\";s7.workTypeDescription=\"Additional work type\";\n" +
+            "s8.workType=\"520\";s8.workTypeDescription=\"This is also some type of work\";\n" +
+            "s9.workType=\"530\";s9.workTypeDescription=\"Work work work\";\n" +
+            "s10.workType=\"550\";s10.workTypeDescription=\"Typing\";\n" +
+            "s11.workType=\"550\";s11.workTypeDescription=\"More typing\";\n" +
+            "s12.workType=\"550\";s12.workTypeDescription=\"Thinking of more descriptions\";\n" +
+            "dwr.engine._remoteHandleCallback('4','0',{dayEnabledStatus:s0,workTypeWorkGroupList:s1});";
 
     public void testRegularInput() {
         List<WorkType> result = WorkTypeListParser.parse(INPUT_REGULAR);
 
         // list of projects
-        assertEquals(5, result.size());
+        assertEquals(11, result.size());
         assertEquals(new WorkType("400", "Fixed Price Consultancy"), result.get(0));
         assertEquals(new WorkType("420", "Consultancy on Maintenance"), result.get(1));
         assertEquals(new WorkType("430", "Free work/ cost for  Xebia"), result.get(2));
--- app/src/instrumentTest/java/com/xebia/xtime/test/weekoverview/loader/WeekOverviewParserTest.java
+++ app/src/instrumentTest/java/com/xebia/xtime/test/weekoverview/loader/WeekOverviewParserTest.java
@@ -1,7 +1,10 @@
 package com.xebia.xtime.test.weekoverview.loader;
 
+import com.xebia.xtime.shared.model.Project;
+import com.xebia.xtime.shared.model.TimeCell;
 import com.xebia.xtime.shared.model.TimeSheetRow;
 import com.xebia.xtime.shared.model.WeekOverview;
+import com.xebia.xtime.shared.model.WorkType;
 import com.xebia.xtime.weekoverview.loader.WeekOverviewParser;
 
 import junit.framework.TestCase;
@@ -10,73 +13,63 @@ import java.util.Date;
 
 public class WeekOverviewParserTest extends TestCase {
 
-    private static final String INPUT_UNPARSEABLE = "this is intentionally not parseable";
     private static final String INPUT_REGULAR = "throw 'allowScriptTagRemoting is false.';\n" +
             "//#DWR-INSERT\n" +
             "//#DWR-REPLY\n" +
-            "var s0=[];var s3={};var s4={};var s5={};var s6={};var s7={};var s8={};var s9={};var " +
-            "s10={};var s11={};var s12={};var s1=[];var s13={};var s15=[];var s16={};var s14={};" +
-            "var s17=[];var s18={};var s2=[];s0[0]=s3;s0[1]=s4;s0[2]=s5;s0[3]=s6;s0[4]=s7;" +
-            "s0[5]=s8;s0[6]=s9;s0[7]=s10;s0[8]=s11;s0[9]=s12;\n" +
+            "var s0=[];var s3={};var s4={};var s5={};var s6={};var s7={};var s8={};var s9={};var s10={};var s11={};var s12={};var s13={};var s1=[];var s14={};var s15={};var s16=[];var s17={};var s18={};var s19={};var s20=[];var s21={};var s2=[];s0[0]=s3;s0[1]=s4;s0[2]=s5;s0[3]=s6;s0[4]=s7;s0[5]=s8;s0[6]=s9;s0[7]=s10;s0[8]=s11;s0[9]=s12;s0[10]=s13;\n" +
             "s3.description=\"PROJECT 1\";s3.id=\"0001\";\n" +
-            "s7.description=\"PROJECT 2\";s7.id=\"0002\";\n" +
-            "s12.description=\"XKE\";s12.id=\"0062\";\n" +
-            "s1[0]=s13;s1[1]=s14;\n" +
-            "s13.clientName=\"CLIENT 1\";s13.description=\"description\";s13.projectId=\"0001\";" +
-            "s13.projectName=\"PROJECT 1\";s13.timeCells=s15;s13.userId=\"1234567\";s13" +
-            ".workTypeDescription=\"Work work\";s13.workTypeId=\"100\";\n" +
-            "s15[0]=s16;\n" +
-            "s16.approved=true;s16.entryDate=new Date(11111);s16.fromAfas=false;s16.hour=\"7.0\";" +
-            "s16.transferredToAfas=false;\n" +
-            "s14.clientName=\"CLIENT 2\";s14.description=\"\";s14.projectId=\"0002\";s14" +
-            ".projectName=\"PROJECT 2\";s14.timeCells=s17;s14.userId=\"1234567\";s14" +
-            ".workTypeDescription=\"Work work\";s14.workTypeId=\"100\";\n" +
-            "s17[0]=s18;\n" +
-            "s18.approved=true;s18.entryDate=new Date(22222);s18.fromAfas=false;s18.hour=\"1.0\";" +
-            "s18.transferredToAfas=false;\n" +
-            "s2[0]=21;s2[1]=22;\n" +
-            "dwr.engine._remoteHandleCallback('0','0',{lastTransferredDate:new Date" +
-            "(1385766000000),monthDaysCount:7,monthlyDataApproved:true," +
-            "monthlyDataTransferred:false,projects:s0,timesheetRows:s1,userName:\"Foo Bar\"," +
-            "weekEndDates:s2,weekStart:null});";
+            "s4.description=\"PROJECT 2\";s4.id=\"0002\";\n" +
+            "s5.description=\"PROJECT 3\";s5.id=\"0003\";\n" +
+            "s6.description=\"PROJECT 4\";s6.id=\"0004\";\n" +
+            "s7.description=\"PROJECT 5\";s7.id=\"0005\";\n" +
+            "s8.description=\"PROJECT 6\";s8.id=\"0006\";\n" +
+            "s9.description=\"PROJECT 7\";s9.id=\"0007\";\n" +
+            "s10.description=\"Internal projects\";s10.id=\"1\";\n" +
+            "s11.description=\"Pre-sales werkzaamheden\";s11.id=\"0073\";\n" +
+            "s12.description=\"TECHRALLY\";s12.id=\"0072\";\n" +
+            "s13.description=\"XKE\";s13.id=\"0062\";\n" +
+            "s1[0]=s14;s1[1]=s15;\n" +
+            "s14.clientName=\"Xebia Internal\";s14.description=\"Holiday\";s14.projectId=\"1\";s14.projectName=\"Internal Project\";s14.timeCells=s16;s14.userId=\"1015900\";s14.workTypeDescription=\"Holiday\";s14.workTypeId=\"950\";\n" +
+            "s16[0]=s17;s16[1]=s18;s16[2]=s19;\n" +
+            "s17.approved=true;s17.entryDate=new Date(1388358000000);s17.fromAfas=true;s17.hour=\"8.0\";s17.transferredToAfas=false;\n" +
+            "s18.approved=true;s18.entryDate=new Date(1388444400000);s18.fromAfas=true;s18.hour=\"8.0\";s18.transferredToAfas=false;\n" +
+            "s19.approved=false;s19.entryDate=new Date(1388530800000);s19.fromAfas=false;s19.hour=\"8.0\";s19.transferredToAfas=false;\n" +
+            "s15.clientName=\"CLIENT X\";s15.description=\"\";s15.projectId=\"0001\";s15.projectName=\"PROJECT 1\";s15.timeCells=s20;s15.userId=\"1015900\";s15.workTypeDescription=\"Work work work\";s15.workTypeId=\"400\";\n" +
+            "s20[0]=s21;\n" +
+            "s21.approved=false;s21.entryDate=new Date(1388617200000);s21.fromAfas=false;s21.hour=\"7.0\";s21.transferredToAfas=false;\n" +
+            "s2[0]=4;s2[1]=5;\n" +
+            "dwr.engine._remoteHandleCallback('0','0',{lastTransferredDate:new Date(1388444400000),monthDaysCount:7,monthlyDataApproved:true,monthlyDataTransferred:false,projects:s0,timesheetRows:s1,userName:\"Foo Bar\",weekEndDates:s2,weekStart:null});\n";
 
     public void testRegularInput() {
         WeekOverview result = WeekOverviewParser.parse(INPUT_REGULAR);
 
         // list of projects
-        assertEquals(3, result.getProjects().size());
-        assertEquals("PROJECT 1", result.getProjects().get(0).getName());
-        assertEquals("0001", result.getProjects().get(0).getId());
-        assertEquals("PROJECT 2", result.getProjects().get(1).getName());
-        assertEquals("0002", result.getProjects().get(1).getId());
-        assertEquals("XKE", result.getProjects().get(2).getName());
-        assertEquals("0062", result.getProjects().get(2).getId());
+        assertEquals(11, result.getProjects().size());
+        assertEquals(new Project("0001", "PROJECT 1"), result.getProjects().get(0));
+        assertEquals(new Project("0002", "PROJECT 2"), result.getProjects().get(1));
+        assertEquals(new Project("0062", "XKE"), result.getProjects().get(10));
 
         // list of time sheet rows
         assertEquals(2, result.getTimeSheetRows().size());
         TimeSheetRow row = result.getTimeSheetRows().get(0);
-        assertEquals("description", row.getDescription());
-        assertEquals("PROJECT 1", row.getProject().getName());
-        assertEquals("0001", row.getProject().getId());
-        assertEquals("Work work", row.getWorkType().getDescription());
-        assertEquals("100", row.getWorkType().getId());
-        assertEquals(1, row.getTimeCells().size());
-        assertEquals(new Date(11111), row.getTimeCells().get(0).getEntryDate());
+        assertEquals("Holiday", row.getDescription());
+        assertEquals(new Project ("1", "Internal Project"), row.getProject());
+        assertEquals(new WorkType("950", "Holiday"), row.getWorkType());
+        assertEquals(3, row.getTimeCells().size());
+        assertEquals(new TimeCell(new Date(1388358000000l), 8, true), row.getTimeCells().get(0));
 
         row = result.getTimeSheetRows().get(1);
         assertEquals("", row.getDescription());
-        assertEquals("PROJECT 2", row.getProject().getName());
-        assertEquals("0002", row.getProject().getId());
-        assertEquals("Work work", row.getWorkType().getDescription());
-        assertEquals("100", row.getWorkType().getId());
+        assertEquals(new Project("0001", "PROJECT 1"), row.getProject());
+        assertEquals(new WorkType("400", "Work work work"), row.getWorkType());
         assertEquals(1, row.getTimeCells().size());
-        assertEquals(new Date(22222), row.getTimeCells().get(0).getEntryDate());
+        assertEquals(new TimeCell(new Date(1388617200000l), 7, false), row.getTimeCells().get(0));
 
         assertEquals("Foo Bar", result.getUsername());
     }
 
     public void testUnparseableInput() {
-        WeekOverview result = WeekOverviewParser.parse(INPUT_UNPARSEABLE);
+        WeekOverview result = WeekOverviewParser.parse("this is intentionally not parseable");
         assertNull(result);
     }
 
