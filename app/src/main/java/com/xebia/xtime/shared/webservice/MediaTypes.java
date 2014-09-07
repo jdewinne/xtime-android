cc12a9435b074ffeea36b9739bf12bd0d3b6c9bd
Steven Mulder <steven.mulder@gmail.com>
2014-09-07 15:00:29 +0200

    Added call to get week overview from XTimeWebService.
    We have to explicitly pass a cookie, because cookies are complicated.

--- app/src/main/java/com/xebia/xtime/shared/webservice/MediaTypes.java
+++ app/src/main/java/com/xebia/xtime/shared/webservice/MediaTypes.java
@@ -3,7 +3,7 @@ package com.xebia.xtime.shared.webservice;
 import com.squareup.okhttp.MediaType;
 
 public class MediaTypes {
-    public static final MediaType FORM_URLENCODED = MediaType.parse("application/x-www-form-urlencoded");
-
-    public static final MediaType CONTENT_TYPE_PLAIN = MediaType.parse("text/plain");
+    public static final MediaType FORM_URLENCODED = MediaType.parse
+            ("application/x-www-form-urlencoded");
+    public static final MediaType TEXT_PLAIN = MediaType.parse("text/plain");
 }
--- app/src/main/java/com/xebia/xtime/shared/webservice/XTimeWebService.java
+++ app/src/main/java/com/xebia/xtime/shared/webservice/XTimeWebService.java
@@ -1,22 +1,32 @@
 package com.xebia.xtime.shared.webservice;
 
 import android.net.Uri;
+import android.util.Log;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
+import com.xebia.xtime.BuildConfig;
 
 import java.io.IOException;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.util.Calendar;
+import java.util.Date;
 
 public class XTimeWebService {
 
     private static final XTimeWebService INSTANCE = new XTimeWebService();
     private static final Uri BASE_URI = Uri.parse("https://xtime.xebia.com/xtime");
-    private final OkHttpClient mHttpClient = new OkHttpClient();
+    private static final String TAG = "XTimeWebService";
+    private final OkHttpClient mHttpClient;
 
     private XTimeWebService() {
-        // do not instantiate
+        mHttpClient = new OkHttpClient();
+        CookieManager cookieManager = new CookieManager();
+        cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
+        mHttpClient.setCookieHandler(cookieManager);
     }
 
     public static XTimeWebService getInstance() {
@@ -30,17 +40,38 @@ public class XTimeWebService {
      * @throws IOException
      */
     public String login(final String username, final String password) throws IOException {
-        // create request data
+        Log.d(TAG, "Login");
         RequestBody body = new LoginRequestBuilder().username(username).password(password).build();
-        // perform request
-        Request request = new Request.Builder()
-                .url(BASE_URI.buildUpon().appendPath("j_spring_security_check").build().toString())
-                .post(body)
-                .build();
-        Response response = mHttpClient.newCall(request).execute();
+        Response response = doRequest(body, "j_spring_security_check");
         while (null != response.priorResponse()) {
             response = response.priorResponse();
         }
         return response.header("Set-Cookie");
+    }
+
+    public String getWeekOverview(final Date week, final String cookie) throws IOException {
+        if (BuildConfig.DEBUG) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(week);
+            Log.d(TAG, "Get overview for week " + calendar.get(Calendar.WEEK_OF_YEAR));
+        }
+        RequestBody body = new GetWeekOverviewRequestBuilder().week(week).build();
+        Response response = doRequest(body, "dwr/call/plaincall/" +
+                "TimeEntryServiceBean.getWeekOverview.dwr", cookie);
+        return response.isSuccessful() ? response.body().string() : null;
+    }
+
+    private Response doRequest(final RequestBody body, final String path) throws IOException {
+        return doRequest(body, path, "");
+    }
+
+    private Response doRequest(final RequestBody body, final String path,
+                               final String cookie) throws IOException {
+        Request request = new Request.Builder()
+                .url(BASE_URI.buildUpon().appendEncodedPath(path).build().toString())
+                .post(body)
+                .header("Cookie", cookie != null ? cookie : "")
+                .build();
+        return mHttpClient.newCall(request).execute();
     }
 }
